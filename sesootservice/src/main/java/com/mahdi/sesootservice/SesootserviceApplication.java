b647ece06b9a88b3b9610eaeb7f98636b73cf2a4
mtayebi <m72tayebi@gmail.com>
2023-10-12 23:38:25 -0700

    init: initialize project

---
+++ sesootservice/src/main/java/com/mahdi/sesootservice/SesootserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mahdi.sesootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesootserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SesootserviceApplication.class, args);
+    }
+
+}
